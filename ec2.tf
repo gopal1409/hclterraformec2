3f59d8d5f36376587631e756f65036ec708b7640
gopal <gopal1409@gmail.com>
2021-10-07 21:03:23 -0700

    terraform for_each

--- ec2.tf
+++ ec2.tf
@@ -2,9 +2,12 @@
 resource "aws_instance" "ec2_demo"{
     ami = data.aws_ami.amznlinux2.id
     instance_type = var.instance_type
+    #instance_type = var.instance_type_list[1] #for list
+    #instance_type = var.instance_type_map["prod"] #for map
     user_data = file("${path.module}/app1-install.sh")
    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
+   count = 2
     tags = {
-        Name="Ec2 gopal1"
+        "Name"= "count-Demo-${count.index}" 
     }
 }
--- variable.tf
+++ variable.tf
@@ -1,3 +1,4 @@
+#aws region as string
 variable "aws_region" {
   description = "Region in which aws resource need to be created"
   type = string
@@ -7,4 +8,18 @@ variable "instance_type" {
   description = "aws instance type"
   type = string
   default = "t2.micro"
+}
+variable "instance_type_list" {
+  description = "aws instance type"
+  type = string
+  default = ["t3.micro","t2.micro","t3.large"]
+}
+variable "instance_type_map" {
+  description = "ec2 instance type"
+  type = map(string)
+  default = {
+    "dev" = "t3.micro"
+    "qa" = "t3.small"
+    "prod" = "t3.large"
+  }
 }
